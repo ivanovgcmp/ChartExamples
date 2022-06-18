a49baf862797d986a0bc243ebbeac7d081c1d8f5
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-18 23:11:48 +0300

    Creating function close program

--- ChartExamples.py
+++ ChartExamples.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
+# Creating function close program
+def do_Close():
+	window.destroy()
+
 # Creating main window
 window = tk.Tk()
 window.geometry("450x450")
 window.title("Examples of plotting")
 
 # Creating button close program
-btnClose = tk.Button(window, text="Close", font = ('Helvetica', 10, 'bold'))
+btnClose = tk.Button(window, text="Close", font = ('Helvetica', 10, 'bold'), command=do_Close)
 btnClose.place(x=330, y=400, width=90, height=30)
 
 # Starting the cycle

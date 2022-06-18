b34d9e2e65156cb6ad7df60bc47d87edfb88fdad
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-18 23:08:26 +0300

    Creating button close program

--- ChartExamples.py
+++ ChartExamples.py
@@ -5,5 +5,9 @@ window = tk.Tk()
 window.geometry("450x450")
 window.title("Examples of plotting")
 
+# Creating button close program
+btnClose = tk.Button(window, text="Close", font = ('Helvetica', 10, 'bold'))
+btnClose.place(x=330, y=400, width=90, height=30)
+
 # Starting the cycle
 window.mainloop()

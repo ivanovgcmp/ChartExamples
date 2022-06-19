3e5df6647a669cfd8d8c6d6274b72138fa67065b
ivanovgcmp <107063363+ivanovgcmp@users.noreply.github.com>
2022-06-19 15:32:47 +0300

    Update Chart1.py

--- Chart1.py
+++ Chart1.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Data for plotting
-def 
+def plot_chart():
     t = np.arange(0.0, 2.0, 0.01)
     s = 1 + np.sin(2 * np.pi * t)
     fig, ax = plt.subplots()
